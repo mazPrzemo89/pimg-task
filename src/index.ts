1511e191e2f1d5a0a15a941ced59213ac1e941df
Przemyslaw Mazur <maz89prz@gmail.com>
2023-06-01 20:11:40 +0100

    removed redundant code from index file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import 'dotenv/config'
 import bodyParser from "body-parser";
 import { router as uploadRouter } from './routers/upload'
@@ -6,14 +6,9 @@ import { router as downloadRouter } from './routers/download'
 
 const app = express();
 app.use(bodyParser.json());
+
 app.use(uploadRouter);
 app.use(downloadRouter);
-
-app.post('/', function (req: Request, res: Response) {
-  const text = req.body.text;
-  console.log(text);
-    res.status(200).send(text);
-});
   
 app.listen(3000, () => {
     console.log("App listening on port 3000!!")
